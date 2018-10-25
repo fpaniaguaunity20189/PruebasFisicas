207b426f3b0eae90833515a3a926f60242f6f7f0
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2018-10-25 14:02:30 +0200

    2018-10-25

---
+++ Assets/_GameAssets/Scripts/AvionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvionScript : MonoBehaviour {
+    public int speed = 100;
+
+	void Update () {
+        float vPos = Input.GetAxis("Vertical");
+        float hPos = Input.GetAxis("Horizontal");
+        //AVANZAR SIN PULSAR NADA
+        GetComponent<CharacterController>().Move(
+            transform.forward * 
+            Time.deltaTime  *
+            speed
+        );
+        //ROTAMOS EL AVION HACIA ARRIBA Y HACIA ABAJO
+        transform.Rotate(new Vector3(vPos, hPos, hPos*-1));
+
+        /*
+         * PARA UN ELEMENTO TERRESTRE
+        GetComponent<CharacterController>().SimpleMove(
+            Vector3.forward * 
+            Time.deltaTime * 
+            speed *
+            vPos);
+        */
+
+	}
+}
--- Assets/_GameAssets/Scripts/TroncomovilScript.cs
+++ Assets/_GameAssets/Scripts/TroncomovilScript.cs
@@ -1,36 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TroncomovilScript : MonoBehaviour {
+    public Text txtFrenoMano;
+    public Text txtSpeed;
+    public Text txtMarcha;
     public float fuerzaMaximaMotor = 100;
     public float anguloMaximoRotacion = 20;
     public float incrementoFrenado = 10;
     private float fuerzaFrenado = 0;
     private float vPos;
     private float hPos;
-    
+    bool frenoManoActivo = true;
+    float fSpeed;
     private WheelCollider wcFrontL, wcFrontR, wcBackL, wcBackR;
+
+
     private void Start() {
+        //Material matPilotos = GetComponentInChildren<Material>();
+        //matPilotos.SetColor("_EmissionColor", Color.red);
+
+
         wcFrontL = GameObject.Find("FrontL").GetComponent<WheelCollider>();
         wcFrontR = GameObject.Find("FrontR").GetComponent<WheelCollider>();
         wcBackL = GameObject.Find("BackL").GetComponent<WheelCollider>();
         wcBackR = GameObject.Find("BackR").GetComponent<WheelCollider>();
     }
+
+    private void Update() {
+        fSpeed = (int)GetComponent<Rigidbody>().velocity.magnitude;
+        txtSpeed.text = ((int)fSpeed).ToString();
+
+        if (Input.GetKeyDown(KeyCode.F)) {
+            frenoManoActivo = !frenoManoActivo;
+            if (frenoManoActivo == true) {
+                txtFrenoMano.text = "Handbrake: ON";
+            } else {
+                txtFrenoMano.text = "Handbrake: OFF";
+            }
+        }
+    }
+
+
     private void FixedUpdate() {
+        //RECOGER COORDENADAS
         vPos = Input.GetAxis("Vertical");
         hPos = Input.GetAxis("Horizontal");
-        if (vPos > 0) {
-            //RUEDAS MOTRICES
-            SoltarFreno();
-            wcBackL.motorTorque = fuerzaMaximaMotor * vPos;
-            wcBackR.motorTorque = fuerzaMaximaMotor * vPos;
-        } else if (vPos < 0) {
-            Frenar();
-        }
+
         //RUEDAS DIRECTRICES
         wcFrontL.steerAngle = anguloMaximoRotacion * hPos;
         wcFrontR.steerAngle = anguloMaximoRotacion * hPos;
+
+        if (!frenoManoActivo) {
+            if (vPos > 0) {
+                txtMarcha.text = "1";
+                //RUEDAS MOTRICES
+                SoltarFreno();
+                wcBackL.motorTorque = fuerzaMaximaMotor * vPos;
+                wcBackR.motorTorque = fuerzaMaximaMotor * vPos;
+            } else if (vPos < 0 && fSpeed>0.0001) {
+                txtMarcha.text = "0";
+                Frenar();
+            } else if (vPos < 0 && fSpeed<=0.0001) {
+                txtMarcha.text = "R";
+                SoltarFreno();
+                wcBackL.motorTorque = fuerzaMaximaMotor * vPos;
+                wcBackR.motorTorque = fuerzaMaximaMotor * vPos;
+            }
+        } else {
+            wcFrontL.brakeTorque = Mathf.Infinity;
+            wcFrontR.brakeTorque = Mathf.Infinity;
+            wcBackL.brakeTorque = Mathf.Infinity;
+            wcBackR.brakeTorque = Mathf.Infinity;
+        }
     }
 
     private void Frenar() {
