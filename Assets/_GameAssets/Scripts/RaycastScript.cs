6629b1ad574af803f8308be8db4e3f806836fcd2
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2018-10-26 13:57:07 +0200

    2018-10-26-FIN SESION

---
+++ Assets/_GameAssets/Scripts/RaycastScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaycastScript : MonoBehaviour {
+    public Transform origen;
+    public Transform destino;
+    public Text texto;
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Disparar();
+        }
+	}
+    private void Disparar() {
+        bool hayImpacto = Physics.Raycast(origen.position, origen.forward);
+        Debug.DrawRay(origen.position, origen.forward, Color.red, 50);
+        texto.text = ""+hayImpacto;
+    }
+}
